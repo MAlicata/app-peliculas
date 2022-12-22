4566e3f03eba357d14506ed9f54396c8678ff602
MAlicata <matiasalicata@gmail.com>
2022-12-22 00:51:23 -0300

    add json file

--- src/App.js
+++ src/App.js
@@ -2,31 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
+import peliculasJson from './peliculas.json';
 
 function App() {
-  let peliculas = [
-    {
-      titulo: "oblivion (2012)",
-      calificacion: "8.1",
-      director:"Joss Whedon",
-      actores:"Robert Downey Jr., Chris Evans, Chris Hemsworth",
-      fecha:"1 May 2015", 
-      duracion:"2h21’", 
-      img:"images/uploads/mv1.jpg",
-      descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-    },
-    {
-      titulo: "oblivion (2012)",
-      calificacion: "8.1",
-      director:"Joss Whedon",
-      actores:"Robert Downey Jr., Chris Evans, Chris Hemsworth",
-      fecha:"1 May 2015", 
-      duracion:"2h21’", 
-      img:"images/uploads/mv1.jpg",
-      descripcion:"Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
-
-    }
-  ];
+  let peliculas = peliculasJson;
   return (   
     <PageWrapper>
       {peliculas.map(pelicula =>
