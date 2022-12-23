2f325e0ce000e91cdeb7e06c191d9fdf06c6b0e7
MAlicata <matiasalicata@gmail.com>
2022-12-23 12:57:05 -0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -3,9 +3,31 @@ import './App.css';
 import Pelicula from './Pelicula';
 import PageWrapper from './PageWrapper';
 import peliculasJson from './peliculas.json';
+import Paginacion from './Paginacion';
+import { useState } from 'react';
 
 function App() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const TOTAL_POR_PAGINA = 7;
+
   let peliculas = peliculasJson;
+
+
+  const cargarPeliculas = () => {
+    peliculas = peliculas.slice(
+      (paginaActual - 1) * TOTAL_POR_PAGINA,
+      paginaActual * TOTAL_POR_PAGINA
+    );
+  }
+
+  
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculasJson.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  cargarPeliculas();
   return (   
     <PageWrapper>
       {peliculas.map(pelicula =>
@@ -17,7 +39,10 @@ function App() {
         {pelicula.descripcion}
         </Pelicula>
       )}
-      
+
+    <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+      setPaginaActual(pagina)
+    }}/>
     </PageWrapper>
     
   );
